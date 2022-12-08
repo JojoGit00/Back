cf330df2b30b71c6b4a0b92b3573cc1f4fbf8358
johann <johann.pierrot@gmail.com>
2022-12-08 18:23:10 +0100

    ajout chek mail mdp

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
+const checkEmail = require('../middleware/email');
+const checkPassword = require('../middleware/password');
 
-
-router.post('/signup', userCtrl.signup);
+router.post('/signup', checkEmail, checkPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
